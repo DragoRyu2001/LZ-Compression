01aebaa40c54adda99eb2737263211a973a3280e
sanchith-lila <sanchith.krishnan@lilagames.com>
2025-02-12 18:07:56 +0530

    Updated logic to repeated tuples, and log them to console

--- src/main.zig
+++ src/main.zig
@@ -5,26 +5,59 @@ pub const LZCompression: type = struct {
     pub fn compression(filePath: []const u8) !void {
         var file = try std.fs.openFileAbsolute(filePath, .{});
         defer file.close();
-
-        var buffer: [100]u8 = undefined;
-        try file.seekTo(0);
-        const bytes_read = try file.readAll(&buffer);
-
-        for (buffer, 0..) |byte, index| {
-            var j: usize = index;
-            while (j >= 0) : (j -= 1) {
-                if (buffer[j] == byte) {}
-            }
-        }
+        try compress(file);
     }
-    fn findLongestMatch(buffer: *const []u8, start_index: usize) !LZTuple {
-        search_index: u32 = 0;
-        while(search_index < start_index) : (search_index+=1) {
-            if(buffer[search_index] == buffer[start_index]) {
-                
+    pub fn compress(file: std.fs.File) !void {
+        var buffer: [64000]u8 = undefined;
+        try file.seekTo(0);
+
+        const bytes_read = try file.readAll(&buffer);
+        var bytes_skipped: usize = 0;
+
+        var index: usize = 0;
+        while (index < bytes_read) {
+            const byte = buffer[index];
+            const tuple = try findLongestMatch(buffer, index);
+            if (tuple.match_found) {
+                std.log.debug("Found tuple: {}", .{tuple});
+                index += tuple.match_length;
+                bytes_skipped += tuple.match_length;
+            } else {
+                std.log.debug("Literal: {c}", .{byte});
+                index += 1;
             }
         }
-        return LZTuple{false, 0, 0};
+        std.log.debug("Total Bytes: {} | Bytes skipped: {} | Compressed Bytes: {}", .{ bytes_read, bytes_skipped, bytes_read - bytes_skipped });
+    }
+    fn findLongestMatch(buffer: [64000]u8, start_index: usize) !LZTuple {
+        var search_index: u32 = 0;
+        var longestMatch: LZTuple = LZTuple{ .match_found = false, .match_offset = 0, .match_length = 0 };
+        while (search_index < start_index) : (search_index += 1) {
+            const match: LZTuple = try getLongestMatch(buffer, search_index, start_index);
+
+            if (!match.match_found) {
+                continue;
+            }
+
+            if (match.match_length >= longestMatch.match_length) {
+                longestMatch = match;
+            }
+        }
+        return longestMatch;
+    }
+
+    fn getLongestMatch(buffer: [64000]u8, search_index: u32, start_index: usize) !LZTuple {
+        var i: u32 = 0;
+        while (start_index + i < buffer.len) : (i += 1) {
+            if (buffer[search_index + i] == buffer[start_index + i]) {
+                continue;
+            }
+            break;
+        }
+        return LZTuple{ .match_found = i >= 4, .match_offset = start_index - search_index, .match_length = i };
     }
 };
-pub const LZTuple: type = struct { match_found: bool, match_offset: u32, match_length: u32 };
+pub const LZTuple: type = struct { match_found: bool, match_offset: usize, match_length: usize };
+// pub const LZError = error{
+//     ByteMatchExceeded,
+// };
--- src/root.zig
+++ src/root.zig
@@ -1,10 +1,20 @@
 const std = @import("std");
+const LZCompression = @import("main.zig").LZCompression;
 const testing = std.testing;
 
 export fn add(a: i32, b: i32) i32 {
     return a + b;
 }
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+test "basic encoding data" {
+    testing.log_level = .debug;
+    const file = try std.fs.cwd().createFile(
+        "junk_file.txt",
+        .{ .read = true },
+    );
+    defer file.close();
+
+    try file.writeAll("Hi Guys! this is a test file to check if this log works or not!!\n And the fun part is the more I write in this file the more it should try to optimize this file for me!!\n Hopefully, this does not break anywhere(fingers crossed)");
+    try LZCompression.compress(file);
+    try std.fs.cwd().deleteFile("junk_file.txt");
 }
