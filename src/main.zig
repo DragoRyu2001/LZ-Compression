5bed71a44d22dc3851a7403216c874c02396cb26
Sanchith R Krishnan <sanchith.krishnan@gmail.com>
2025-02-12 08:14:24 +0530

    Added support for reading files

--- src/main.zig
+++ src/main.zig
@@ -13,8 +13,18 @@ pub const LZCompression: type = struct {
         for (buffer, 0..) |byte, index| {
             var j: usize = index;
             while (j >= 0) : (j -= 1) {
-
+                if (buffer[j] == byte) {}
             }
         }
     }
+    fn findLongestMatch(buffer: *const []u8, start_index: usize) !LZTuple {
+        search_index: u32 = 0;
+        while(search_index < start_index) : (search_index+=1) {
+            if(buffer[search_index] == buffer[start_index]) {
+                
+            }
+        }
+        return LZTuple{false, 0, 0};
+    }
 };
+pub const LZTuple: type = struct { match_found: bool, match_offset: u32, match_length: u32 };
